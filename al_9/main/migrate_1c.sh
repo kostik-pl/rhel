be8d6c400aeaf7f010ef4eafde7c4057aa8785bb
kostik-pl <kostik_pl@hotmail.com>
2025-01-21 09:27:49 +0200

    Update migrate_1c.sh

--- al_9/main/migrate_1c.sh
+++ al_9/main/migrate_1c.sh
@@ -22,7 +22,7 @@ then
     #Manual installation the process will run witout options
     ./$file_name --mode unattended --unattendedmodeui minimal --disable-components client_full,client_thin,client_thin_fib,config_storage_server,liberica_jre,integrity_monitoring --enable-components server,server_admin,ws,additional_admin_functions,v8_install_deps,uk,ru
 
-    if [ -f '/opt/1cv8/x86_64/'$old'/usr1cv8.keytab ' ]
+    if [ -f '/opt/1cv8/x86_64/'$old'/usr1cv8.keytab' ]
     then 
         yes | cp -v '/opt/1cv8/x86_64/'$old'/usr1cv8.keytab' /_data/usr1cv8.keytab
         chown usr1cv8:grp1cv8 /_data/usr1cv8.keytab
@@ -33,7 +33,7 @@ then
     then
         echo 'DEBUG already ENABLED'
     else
-	    sed -ri 's/Environment=SRV1CV8_DEBUG=/Environment=SRV1CV8_DEBUG=-debug/' /opt/1cv8/x86_64/$new/srv1cv8-$new@.service
+        sed -ri 's/Environment=SRV1CV8_DEBUG=/Environment=SRV1CV8_DEBUG=-debug/' /opt/1cv8/x86_64/$new/srv1cv8-$new@.service
     fi
     sed -ri 's/Environment=SRV1CV8_DATA=\/home\/usr1cv8\/.1cv8\/1C\/1cv8/Environment=SRV1CV8_DATA=\/_data\/srv1c_inf_log/' /opt/1cv8/x86_64/$new/srv1cv8-$new@.service
     sed -ri 's/Environment=SRV1CV8_KEYTAB=\/opt\/1cv8\/x86_64\/'$new'\/usr1cv8.keytab/Environment=SRV1CV8_KEYTAB=\/_data\/usr1cv8.keytab/' /opt/1cv8/x86_64/$new/srv1cv8-$new@.service
