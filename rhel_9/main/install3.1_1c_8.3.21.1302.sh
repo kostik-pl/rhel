42e64778650e56d5167f79509284801c2bccd5cc
kostik-pl <kostik_pl@hotmail.com>
2025-01-29 16:56:29 +0200

    UPDATE

--- rhel_9/main/install3.1_1c_8.3.21.1302.sh
+++ rhel_9/main/install3.1_1c_8.3.21.1302.sh
@@ -22,30 +22,33 @@ dnf install -y libgsf
 dnf install -y epel-release
 dnf install -y ImageMagick
 
-#Install 1C Enterprise server requirements from custom packages
-curl "https://drive.usercontent.google.com/download?id=1-6UeVusRsqn33AAmAozG_NH-CmHDwKMx&confirm=xxx" -o msttcorefonts-2.5-1.noarch.rpm
-rpm -ivh msttcorefonts-2.5-1.noarch.rpm
+#Install 1C Enterprise server requirements from FONTS packages
+dnf --enablerepo=ol9_distro_builder install xorg-x11-font-utils
+rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+#Previous version
+#curl "https://drive.usercontent.google.com/download?id=1-6UeVusRsqn33AAmAozG_NH-CmHDwKMx&confirm=xxx" -o msttcorefonts-2.5-1.noarch.rpm
+#rpm -ivh msttcorefonts-2.5-1.noarch.rpm
 fc-cache /usr/share/fonts
 
 #Install 1C Enterprise server packages from work dir
 #Download form GOOGLE
-curl "https://drive.usercontent.google.com/download?id=1ZsCOPrMJl5fU0r4tsEPE0C7tluO-i3zX&confirm=xxx" -o setup-full-8.3.25.1286-x86_64.run
-chmod +x setup-full-8.3.25.1286-x86_64.run
+curl "https://drive.usercontent.google.com/download?id=1eYMu7e0KNAfByJMZqjNsUPjf3slyQBxJ&confirm=xxx" -o setup-full-8.3.21.1302-x86_64.run
+chmod +x setup-full-8.3.21.1302-x86_64.run
 #ATTENTION! Batch installation will always install the 1c client and, if missing, the trimmed GNOME
-./setup-full-8.3.25.1286-x86_64.run --mode unattended --unattendedmodeui minimal --disable-components client_full,client_thin,client_thin_fib,config_storage_server,liberica_jre,integrity_monitoring --enable-components server,server_admin,ws,additional_admin_functions,v8_install_deps,uk,ru
+./setup-full-8.3.21.1302-x86_64.run --mode unattended --unattendedmodeui minimal --disable-components client_full,client_thin,client_thin_fib,config_storage_server,liberica_jre,integrity_monitoring --enable-components server,server_admin,ws,additional_admin_functions,uk,ru
 #Manual installation, if have GUI (GNOME), the process will run in it
-#./setup-full-8.3.25.1286-x86_64.run
+#./setup-full-8.3.21.1302-x86_64.run
 
-sed -ri 's/Environment=SRV1CV8_DEBUG=/Environment=SRV1CV8_DEBUG=-debug/' /opt/1cv8/x86_64/8.3.25.1286/srv1cv8-8.3.25.1286@.service
-sed -ri 's/Environment=SRV1CV8_DATA=\/home\/usr1cv8\/.1cv8\/1C\/1cv8/Environment=SRV1CV8_DATA=\/_data\/srv1c_inf_log/' /opt/1cv8/x86_64/8.3.25.1286/srv1cv8-8.3.25.1286@.service
+sed -ri 's/Environment=SRV1CV8_DEBUG=/Environment=SRV1CV8_DEBUG=-debug/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
+sed -ri 's/Environment=SRV1CV8_DATA=\/home\/usr1cv8\/.1cv8\/1C\/1cv8/Environment=SRV1CV8_DATA=\/_data\/srv1c_inf_log/' /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
 sed -ri 's/Environment=SRV1CV8_KEYTAB=\/opt\/1cv8\/x86_64\/8.3.25.1286\/usr1cv8.keytab/Environment=SRV1CV8_KEYTAB=\/_data\/usr1cv8.keytab/' /opt/1cv8/x86_64/8.3.25.1286/srv1cv8-8.3.25.1286@.service
 
-systemctl link /opt/1cv8/x86_64/8.3.25.1286/srv1cv8-8.3.25.1286@.service
-systemctl link /opt/1cv8/x86_64/8.3.25.1286/ras-8.3.25.1286.service
-systemctl enable srv1cv8-8.3.25.1286@default
-systemctl enable ras-8.3.25.1286
-systemctl start srv1cv8-8.3.25.1286@default
-systemctl start ras-8.3.25.1286
+systemctl link /opt/1cv8/x86_64/8.3.21.1302/srv1cv8-8.3.21.1302@.service
+systemctl link /opt/1cv8/x86_64/8.3.21.1302/ras-8.3.21.1302.service
+systemctl enable srv1cv8-8.3.21.1302@default
+systemctl enable ras-8.3.21.1302
+systemctl start srv1cv8-8.3.21.1302@default
+systemctl start ras-8.3.21.1302
 
 curl "https://drive.usercontent.google.com/download?id=1RND475Sc9MquDt9W1At9rMQP_N6OOfSk&confirm=xxx" -o /etc/firewalld/services/srv1c.xml
 firewall-cmd --reload
