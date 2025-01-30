ee9502f7befabfcee64b70c5018c19b46db5174d
kostik-pl <kostik_pl@hotmail.com>
2025-01-30 04:03:09 +0200

    UPDATE

--- al_9/pgpro_std_13/container-entrypoint.sh
+++ al_9/pgpro_std_13/container-entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-chmod 700 $PGDATA
-chmod 700 /_data/pg_backup
+chown -R postgres:postgres $PGDATA ; \
+chmod -R 700 $PGDATA ; \
+chown -R postgres:postgres /_data/pg_backup ; \
+chmod 777 /_data/pg_backup
 
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     pg_ctl initdb -D $PGDATA -o "--locale=$LANG --lc-collate=$LANG"
