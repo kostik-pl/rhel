e2f5943496e953f67c564aacd798f8a9e23f67a8
kostik-pl <kostik_pl@hotmail.com>
2026-03-24 09:52:31 +0200

    UPDATE

---
+++ al_9/pgpro_std_13_ru/container-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+chown -R postgres:postgres $PGDATA ; \
+chmod -R 700 $PGDATA ; \
+chown -R postgres:postgres /_data/pg_backup ; \
+chmod 777 /_data/pg_backup
+
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+    pg_ctl initdb -D $PGDATA -o "--locale=ru_RU.UTF-8 --lc-collate=ru_RU.UTF-8"
+    printf "host    all             all             all                     md5\n" >> $PGDATA/pg_hba.conf
+fi
+
+exec "$@"
